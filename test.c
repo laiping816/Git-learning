aecf2f6645c221bc10eb6ba639448ed0268dd39a
laiping816 <39050509+laiping816@users.noreply.github.com>
2018-06-12 00:08:11 +0800

    github remote

--- test.c
+++ test.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 void main(){
    printf("helloworld");
-   printf("5nd edit"); 
-   printf("mybranch2");
-   printf("mybranch1");
-   printf("mybranch1-2");
-   printf("local edit");
-   printf("github 2 remote");
+   printf("github remote");
 }
