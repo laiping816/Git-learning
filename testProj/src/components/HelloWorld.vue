9ff1f949afc6a06cad28a6ed0e8a0f7b1e5a216f
laiping <634042362@qq.com>
2018-06-12 19:16:04 +0800

    添加vue-cli测试demoProj

---
+++ testProj/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="login">
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="text">文字按钮</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+      login(){}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
