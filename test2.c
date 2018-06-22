0a334ca4ed4e5d0a9cbe94d5830c63b36a5c617b
laiping816 <39050509+laiping816@users.noreply.github.com>
2018-06-22 13:55:20 +0800

    remote 622

--- test2.c
+++ test2.c
@@ -3,9 +3,6 @@
 void main(int args,char *args[]){
    printf("branch testing");
    printf("local  edit");
-<<<<<<< HEAD
    printf("local  edit 622");
-=======
    printf("remoto  edit");
->>>>>>> a4e09ae99faf36385843f2946bf01eeff6bb0d19
 }
