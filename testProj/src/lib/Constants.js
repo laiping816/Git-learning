2b8df6ad41b964b1141d14d71a02e7828b4bf531
laiping <634042362@qq.com>
2018-08-08 14:24:50 +0800

    wwewe

--- testProj/src/lib/Constants.js
+++ testProj/src/lib/Constants.js
@@ -4,3 +4,32 @@ export const DayType = {
   Weekend: '休息日',
   Holiday: '节假日'
 };
+export const WeekDays = [{
+  value: 0,
+  name: '星期日',
+  shortName: '日'
+},{
+  value: 1,
+  name: '星期一',
+  shortName: '一'
+},{
+  value: 2,
+  name: '星期二',
+  shortName: '二'
+},{
+  value: 3,
+  name: '星期三',
+  shortName: '三'
+},{
+  value: 4,
+  name: '星期四',
+  shortName: '四'
+},{
+  value: 5,
+  name: '星期五',
+  shortName: '五'
+},{
+  value: 6,
+  name: '星期六',
+  shortName: '六'
+}];
--- testProj/src/pages/Index.vue
+++ testProj/src/pages/Index.vue
@@ -1,4 +1,5 @@
 <template>
+  <div>
   <el-select v-model="value5" filterable multiple placeholder="请选择">
     <el-option
       v-for="item in options"
@@ -7,10 +8,13 @@
       :value="item.value">
     </el-option>
   </el-select>
+  <el-button type="primary" @click="momentTest">主要按钮</el-button>
+  </div>
 </template>
 
 <script>
   import moment from 'moment'
+  import { WeekDays} from "../lib/Constants";
   import _ from 'lodash'
   export default {
     components: {
@@ -26,6 +30,57 @@
           label: '双皮奶'
         }],
         value5: ['选项2'],
+      }
+    },
+    methods:{
+      weekday: function(date) {
+        let dayOfWeek = moment(date).day();
+
+        return {
+          name: WeekDays[dayOfWeek].name,
+          date: date,
+          dayOfWeek: dayOfWeek,
+          isWeekend: dayOfWeek === 0 || dayOfWeek === 6
+        };
+      },
+      weekOfMonth(date) {
+        let momDate = moment(date);
+        if (momDate.day() === 0) {
+          momDate = momDate.subtract(2, 'days');
+          date = new Date(momDate.year(), momDate.month(), momDate.date());
+        }
+
+        let weekStart = moment(date).weekday(1);
+        let weekEnd = moment(date).weekday(7);
+        let weekMiddle = moment(date).weekday(4);
+
+        // console.log(`date=${date} momDate=${momDate.format('YYYY-MM-DD')} weekStart=${weekStart.format('YYYY-MM-DD')} weekEnd=${weekEnd.format('YYYY-MM-DD')}`);
+
+        let weekSeq = 0;
+        if (weekMiddle.month() != weekStart.month()) {
+          weekSeq = 1;
+        } else {
+          let firstWeekStart = moment(date).weekday(4).date(1).weekday(1);
+          let firstWeekMiddle = moment(date).weekday(4).date(1).weekday(4);
+          if (firstWeekMiddle.month() != weekMiddle.month()) {
+            firstWeekMiddle = moment(date).weekday(4).date(1).weekday(11);
+          }
+
+          weekSeq = (weekMiddle.date() - firstWeekMiddle.date()) / 7 + 1;
+        }
+
+        return {
+          year: weekMiddle.year(),
+          month: weekMiddle.month() + 1,
+          weekSeq: weekSeq,
+          weekStart: new Date(weekStart.year(), weekStart.month(), weekStart.date()),
+          weekMiddle: new Date(weekMiddle.year(), weekMiddle.month(), weekMiddle.date()),
+          weekEnd: new Date(weekEnd.year(), weekEnd.month(), weekEnd.date(), 23, 59, 59)
+        }
+      },
+      momentTest(){
+        console.log(this.weekday(moment().toDate()));
+        console.log(this.weekOfMonth(moment().toDate()));
       }
     },
     updated(){
